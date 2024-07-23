517effe682c646e6217cfdc39ebc5e61feac4dc7
leandrogilcarrano <leandro.gil@mulesoft.com>
2024-07-23 12:39:51 -0300

    add avro schema and show data type

--- src/ApiTypeDocument.js
+++ src/ApiTypeDocument.js
@@ -560,9 +560,9 @@ export class ApiTypeDocument extends PropertyDocumentMixin(LitElement) {
     if (Array.isArray(item)) {
       return item;
     }
-
+    
     const propertyKey = this._getAmfKey(this.ns.w3.shacl.property);
-    const itemProperties = this._ensureArray(item[propertyKey])
+    const itemProperties = this._ensureArray(item[propertyKey]||[])
     const additionalPropertiesKey = this._getAmfKey(this.ns.w3.shacl.additionalPropertiesSchema);
 
     // If the item doesn't have additional properties, filter the read-only properties and return
@@ -571,9 +571,9 @@ export class ApiTypeDocument extends PropertyDocumentMixin(LitElement) {
     }
 
     const additionalPropertiesSchema = this._ensureArray(item[additionalPropertiesKey])
-
+    
     // If the item does have additional properties, ensure they are in an array
-    const additionalProperties = this._ensureArray(additionalPropertiesSchema[0][propertyKey])
+    const additionalProperties = this._ensureArray(additionalPropertiesSchema[0][propertyKey] || additionalPropertiesSchema[0])
 
     // Combine the item's properties and additional properties
     const combinedProperties = [...itemProperties, ...additionalProperties]
