c7abdde7c08e56827503564e9d06c3f15afa95ef
Alex Perez <alexperez@mulesoft.com>
2025-12-29 11:39:21 -0300

    feat: add grpc-test.json and update .gitignore
    - Introduced grpc-test.json for gRPC testing configurations.
    - Updated .gitignore to include grpc-test.json for tracking purposes.

--- demo/index.js
+++ demo/index.js
@@ -99,6 +99,8 @@ class ApiDemo extends ApiDemoPage {
 
   _apiListTemplate() {
     return [
+      // grpc
+      ['grpc-test', 'GRPC test'],
       ['shopper-products', 'shopper-products'],
       ['demo-api', 'Demo API'],
       ['avro', 'avro'],
